d1eb761a41b30a5e26d7ce6a1c079db4e3aef96c
Zhangqin <642215734@qq.com>
2019-07-29 16:20:30 +0800

    innitial

---
+++ preprocess.py
@@ -0,0 +1,49 @@
+'''
+
+图像二值化&图像增广
+'''
+
+import d2lzh as d2l
+import mxnet as mx
+from mxnet import autograd, gluon, image, init, nd
+from mxnet.gluon import data as gdata, loss as gloss, utils as gutils
+import sys
+import time
+import numpy as np
+from PIL import Image
+
+path = './data/1.png'
+
+d2l.set_figsize()
+img = image.imread(path)
+
+th = 90
+d2l.plt.imshow(img.asnumpy())
+
+
+def apply(img, aug, num_rows=2, num_cols=4, scale=1.5):
+    Y = [aug(img) for _ in range(num_rows * num_cols)]
+    d2l.show_images(Y, num_rows, num_cols, scale)
+
+
+# apply(img, gdata.vision.transforms.RandomFlipLeftRight())
+# apply(img, gdata.vision.transforms.RandomFlipTopBottom())
+shape_aug = gdata.vision.transforms.RandomResizedCrop(
+    (1200, 300), scale=(0.6, 1), ratio=(0.1, 0.4))
+apply(img, shape_aug)
+transform_test = gdata.vision.transforms.Compose([
+    gdata.vision.transforms.ToTensor(),
+    gdata.vision.transforms.Normalize([0.4914, 0.4822, 0.4465], [0.20230, 19940, 2010])])
+# color_aug = gdata.vision.transforms.RandomColorJitter(
+#     brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5)
+# apply(img, color_aug)
+d2l.plt.show()
+
+img = Image.open(path)
+img = img.convert("L")
+
+WHITE, BLACK = 255, 0
+
+img = img.point(lambda x: WHITE if x > th else BLACK)
+img = img.convert('1')
+img.save(path[-5:])
---
+++ segmentation.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+
+'''
+投影分割，行->列->行
+
+'''
+def segment(path='./5.png', root='./', number=0, dsize=100):
+    # dsize归一化处理的图像大小
+    stop=number+3
+    img = cv2.imread(path)
+    data = np.array(img)
+    len_x = data.shape[0]
+    len_y = data.shape[1]
+    min_val = 10  # 设置最小的文字像素高度，防止切分噪音字符
+
+    start_i = -1
+    end_i = -1
+    rowPairs = []  # 存放每行的起止坐标
+
+    # 行分割
+    for i in range(len_x):
+        if (not data[i].all() and start_i < 0):
+            start_i = i
+        elif (not data[i].all()):
+            end_i = i
+        elif (data[i].all() and start_i >= 0):
+            # print(end_i - start_i)
+            if (end_i - start_i >= min_val):
+                rowPairs.append((start_i, end_i))
+            start_i, end_i = -1, -1
+
+    print(rowPairs)
+
+    # 列分割
+    start_j = -1
+    end_j = -1
+    min_val_word = 5  # 最小文字像素长度
+    # 分割后保存编号
+    for start, end in rowPairs:
+        for j in range(len_y):
+            if (not data[start: end, j].all() and start_j < 0):
+                start_j = j
+            elif (not data[start: end, j].all()):
+                end_j = j
+            elif (data[start: end, j].all() and start_j >= 0):
+                if (end_j - start_j >= min_val_word):
+                    # print(end_j - start_j)
+                    tmp = data[start:end, start_j: end_j]
+                    # cv2.imshow('demo', tmp)
+                    # cv2.waitKey(0)
+                    # print(tmp.shape)
+
+                    start_i = -1
+                    end_i = -1
+                    for i in range(tmp.shape[0]):
+                        if (not tmp[i].all() and start_i < 0):
+                            start_i = i
+                        elif (not tmp[i].all()):
+                            end_i = i
+                        elif (tmp[i].all() and start_i >= 0):
+                            # print(end_i - start_i)
+                            if (end_i - start_i >= min_val):
+                                pass
+
+                    tmp = tmp[start_i:end_i, :]
+                    cv2.imshow('demo', tmp)
+                    cv2.waitKey(0)
+
+                    im2save = cv2.resize(tmp, (dsize, dsize))  # 归一化处理
+                    cv2.imwrite(root + '/%d.png' % number, im2save)
+                    number += 1
+                    print("%d  pic" % number)
+                    # 只要前三个字
+                    if number == stop:
+                        break
+                start_j, end_j = -1, -1
+
+
+if __name__ == '__main__':
+    segment('./1.png', './data/train',14)
