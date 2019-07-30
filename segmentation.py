37145a91c843121201ec073706c5912eadfae8dd
Zhangqin <642215734@qq.com>
2019-07-30 14:17:47 +0800

    exif 倾斜矫正

--- segmentation.py
+++ segmentation.py
@@ -6,14 +6,14 @@ import numpy as np
 投影分割，行->列->行
 
 '''
-def segment(path='./5.png', root='./', number=0, dsize=100):
+def segment(path='./in.png', root='./', number=0, dsize=100):
     # dsize归一化处理的图像大小
     stop=number+3
     img = cv2.imread(path)
     data = np.array(img)
     len_x = data.shape[0]
     len_y = data.shape[1]
-    min_val = 10  # 设置最小的文字像素高度，防止切分噪音字符
+    min_val = 20  # 设置最小的文字像素高度，防止切分噪音字符
 
     start_i = -1
     end_i = -1
@@ -36,7 +36,7 @@ def segment(path='./5.png', root='./', number=0, dsize=100):
     # 列分割
     start_j = -1
     end_j = -1
-    min_val_word = 5  # 最小文字像素长度
+    min_val_word = 10  # 最小文字像素长度
     # 分割后保存编号
     for start, end in rowPairs:
         for j in range(len_y):
@@ -48,23 +48,24 @@ def segment(path='./5.png', root='./', number=0, dsize=100):
                 if (end_j - start_j >= min_val_word):
                     # print(end_j - start_j)
                     tmp = data[start:end, start_j: end_j]
+
                     # cv2.imshow('demo', tmp)
                     # cv2.waitKey(0)
                     # print(tmp.shape)
 
-                    start_i = -1
-                    end_i = -1
-                    for i in range(tmp.shape[0]):
-                        if (not tmp[i].all() and start_i < 0):
-                            start_i = i
-                        elif (not tmp[i].all()):
-                            end_i = i
-                        elif (tmp[i].all() and start_i >= 0):
-                            # print(end_i - start_i)
-                            if (end_i - start_i >= min_val):
-                                pass
+                    # start_i = -1
+                    # end_i = -1
+                    # for i in range(tmp.shape[0]):
+                    #     if (not tmp[i].all() and start_i < 0):
+                    #         start_i = i
+                    #     elif (not tmp[i].all()):
+                    #         end_i = i
+                    #     elif (tmp[i].all() and start_i >= 0):
+                    #         # print(end_i - start_i)
+                    #         if (end_i - start_i >= min_val):
+                    #             pass
 
-                    tmp = tmp[start_i:end_i, :]
+                    # tmp = tmp[start_i:end_i, :]
                     cv2.imshow('demo', tmp)
                     cv2.waitKey(0)
 
@@ -79,4 +80,4 @@ def segment(path='./5.png', root='./', number=0, dsize=100):
 
 
 if __name__ == '__main__':
-    segment('./7.png', './data/train',17)
+    segment('./in.png', './data/train',20)
